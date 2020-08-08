50ecbdf76b86d0d76891db31ffdf503f4e15a5dd
Grigoriy <slimbde@gmail.com>
2020-08-08 16:09:11 +0500

    dropdown_datetime

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -5,25 +5,25 @@
         <a href="#" @click.prevent="$emit('togglesidebar')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{this.date.toLocaleString("ru")}}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
         <li>
-          <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+          <a class="dropdown-trigger black-text" href="#" data-target="dropdown" ref="dropdown">
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -34,3 +34,30 @@
   </nav>
 </template>
 
+
+<script>
+export default {
+  data: () => ({
+    date: new Date(),
+    timerId: null,
+  }),
+  methods: {
+    logout() {
+      console.log("Logout");
+      this.$router.push("/login?message=logout");
+    },
+  },
+  mounted() {
+    M.Dropdown.init(this.$refs.dropdown, {
+      coverTrigger: false,
+    });
+
+    this.timerId = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.timerId);
+  },
+};
+</script>
